a31d066eebff62afe51051d91982d820f8bf31d7
ayunas <ayunas@gmail.com>
2019-04-05 12:49:28 -0500

    refactored <Header /> to use styled components, first by adding the styled-components dependency in package.json, then importing default styled object from styled-components. Then creating styled components and replacing the <div> and <span>

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,16 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const Divvy = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+`;
+
+const Daty = styled.span`
+  margin-left: 25px;
+  flex: 1;
+`;
+
+const Tempy = styled.span`
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`;
+
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
+    <Divvy className="header">
+      <Daty className="date">SMARCH 32, 2018</Daty>
       <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+      <Tempy className="temp">98°</Tempy>
+    </Divvy>
   )
 }
 
-export default Header
+export default Header;
